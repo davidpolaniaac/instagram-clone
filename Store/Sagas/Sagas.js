b9e9bb9e9ec0bb3431989bb7ace23a414b51b048
David Polania <davidpolaniaac@users.noreply.github.com>
2019-01-21 20:47:31 -0500

    add singup with firebase

--- Store/Sagas/Sagas.js
+++ Store/Sagas/Sagas.js
@@ -1,14 +1,22 @@
 import { takeEvery, call } from 'redux-saga/effects';
-import { autenticacion } from '../Servicios/Firebase';
+import { autenticacion, baseDeDatos } from '../Servicios/Firebase';
 
 const registroEnFirebase = values => autenticacion
   .createUserWithEmailAndPassword(values.correo, values.password)
-  .then(success => success.toJSON())
+  .then(success => success.user.toJSON())
   .catch(error => error);
+
+const registroEnBaseDeDatos = ({ uid, email, nombre }) => baseDeDatos.ref(`usuarios/${uid}`).set({
+  nombre,
+  email,
+});
 
 function* generadoraRegistro(values) {
   try {
     const registro = yield call(registroEnFirebase, values.datos);
+    const { email, uid } = registro;
+    const { datos: { nombre } } = values;
+    yield call(registroEnBaseDeDatos, { uid, email, nombre });
   } catch (error) {
     console.log(error);
   }
